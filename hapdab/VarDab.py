161e841092587d5309c8180ca573c57a84c2d6dd
Rob <schae234@umn.edu>
2017-09-12 21:18:49 -0500

    recover deleted code from vim buffer

--- hapdab/VarDab.py
+++ hapdab/VarDab.py
@@ -1,7 +1,9 @@
 from minus80 import Freezable
 from itertools import chain,repeat
 
+import numpy as np
 import pandas as pd
+import time as time
 
 class VarDab(Freezable):
 
@@ -9,19 +11,50 @@ class VarDab(Freezable):
         super().__init__(name)
         self._initialize_tables()
 
+    def genotypes(self,samples=None,as_dataframe=True,):
+        query = 'SELECT * FROM sample_genotypes '
+        if samples != None:
+            query += 'WHERE sample in ("{}")'.format('","'.join(samples))
 
-    def genotypes(self,gformat='GT',samples=None,loci=None,files=None):
-        return pd.DataFrame(
-            self._db.cursor().execute(
-                'SELECT * FROM sample_genotypes WHERE format = ?',
-                (gformat,)
-            ).fetchall(),
-            columns = ['chrom','pos','alt','ref','sample','format','value']
+        data = self._db.cursor().execute(
+            query,
         )
+        if as_dataframe == True:
+            data = pd.DataFrame(
+                data.fetchall(),
+                columns = ['chrom','pos','alt','ref','sample','dosage','flag']
+            )
+            data['flag'] = [bin(x) for x in data['flag']]
+        return data
+
+    @property
+    def samples(self):
+        return [ x[0] for x in self._db.cursor().execute(
+            "SELECT sample FROM samples ORDER BY sample"        
+        ).fetchall()]
+
+    @property
+    def num_accessions(self):
+        return self._db.cursor().execute(
+            "SELECT COUNT(*) FROM samples"        
+        ).fetchone()[0]
+
+    @property
+    def num_snps(self):
+        return self._db.cursor().execute(
+            "SELECT COUNT(*) FROM variants" 
+        ).fetchone()[0]
+
+    @property
+    def shape(self):
+        return (self.num_snps,self.num_accessions)
 
     def add_VCF(self, filename):
         cur = self._db.cursor()
+        cur.execute('PRAGMA synchronous = off')
+        cur.execute('PRAGMA journal_mode = memory')
         cur.execute('SAVEPOINT add_vcf')
+        start_time = time.time()
         try:
             # Add the filename to the Database
             cur.execute('''
@@ -40,12 +73,15 @@ class VarDab(Freezable):
             with open(filename,'r') as IN:
                 genotypes = []
                 for line_id,line in enumerate(IN):
-                    if line_id % 10_000 == 0:
-                        print(f"Processed {line_id}")
+                    if line_id % 100_000 == 0 and line_id > 0:
                         cur.executemany('''
                             INSERT INTO genotypes VALUES (?,?,?,?,?) 
                         ''',genotypes)
+                        elapsed = time.time() - start_time 
+                        rate = int(100_000 / elapsed)
+                        print(f"Processed {line_id} variants ({rate}/second)")
                         genotypes = []
+                        start_time = time.time()
                     line = line.strip()
                     # Case 1: Header line
                     if line.startswith('##'):
@@ -97,25 +133,69 @@ class VarDab(Freezable):
                         ''',(file_id,var_id,qual,fltr))
                         # Insert the info fields
                         info = [field.split('=') for field in info.split(';')]
-                        fmt = [cur_format[x] for x in fmt.split(':')] 
+                        # Find the Genotype Field Index
+                        GT_ind = fmt.split(':').index('GT')
                         # Insert the genotypes 
-                        genos = [field.split(':') for field in genos]
-                        for geno,sample in zip(genos,cur_samples.values()):
-                            for i,g in enumerate(geno):
-                                genotypes.append((
-                                    file_id,
-                                    var_id,
-                                    sample,
-                                    fmt[i],
-                                    g
-                                ))
+                        for g,sample in zip(genos,cur_samples.values()):
+                            GT = g.split(':')[GT_ind]
+                            genotypes.append((
+                                file_id,
+                                var_id,
+                                sample,
+                                self._GT_to_flag(GT),
+                                self._GT_to_dosage(GT)
+                            ))
             cur.executemany('''
                 INSERT INTO genotypes VALUES (?,?,?,?,?) 
             ''',genotypes)
             cur.execute('RELEASE SAVEPOINT add_vcf')
+            elapsed = time.time() - start_time 
+            rate = int(100_000 / elapsed)
+            print(f"Processed {line_id} variants ({rate}/second)")
+
         except Exception as e:
             cur.execute('ROLLBACK TO SAVEPOINT add_vcf')
+            cur.execute('RELEASE SAVEPOINT add_vcf')
             raise e
+
+    def to_VCF(self,filename):
+        genos = []
+        tab = '\t'
+        cvar = None
+        with open(filename,'w') as OUT:
+            print('##fileformat=VCFv4.1',file=OUT) 
+            print("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{}".format('\t'.join(self.samples)),file=OUT)
+            # Iterate over the records 
+            for record in self.genotypes(as_dataframe=False): 
+                if len(genos) > 0 and (cvar[0],cvar[1]) != (record[0],record[1]):
+                    # Emit a line when you find a new variant
+                    print(
+                        "{}\t{}\t{}\t{}\t{}\t.\t.\t.\tGT\t{}".format(
+                            cvar[0],
+                            cvar[1],
+                            '.',
+                            cvar[2],
+                            cvar[3],
+                            '\t'.join([self._dosage_to_string(x[5]) for x in genos])
+                        ),
+                        file=OUT
+                    )
+                    genos = []
+                genos.append(record)
+                cvar = record 
+            else:
+                #Prin   t out the last one
+                print(  
+                    "{}\t{}\t{}\t{}\t{}\t.\t.\t.\tGT\t{}".format(
+                        cvar[0],
+                        cvar[1],
+                        cvar[2],
+                        cvar[3],
+                        cvar[4],
+                        '\t'.join([self._dosage_to_string(x[5]) for x in genos])
+                    ),
+                    file=OUT
+                )
 
     def _drop_tables(self):
         tables = [x[0] for x in self._db.cursor().execute(
@@ -135,7 +215,113 @@ class VarDab(Freezable):
                     DROP VIEW IF EXISTS {};
                 '''.format(table))
 
+    def _reset_tables(self):
+        self._drop_tables()
+        self._initialize_tables()
 
+    # -----------------------------------------------------------------
+    # Static Methods
+    # -----------------------------------------------------------------
+
+    @staticmethod
+    def _dosage_to_string(dosage):
+        if dosage == 0:
+            return '0/0'
+        elif dosage == 1:
+            return '0/1'
+        else:
+            return '1/1'
+
+    
+    @staticmethod
+    def _GT_to_flag(alleles):
+        flag = 0b0000000
+        # Phased
+        if '|' in alleles:
+            flag = flag | 0b00000001
+        # Heterozygous and 1|0
+        if alleles == '1|0':
+            flag = flag | 0b00000010
+        return int(flag)
+        
+
+    @staticmethod
+    def _GT_to_dosage(alleles):
+        alleles = alleles.replace('|','').replace('/','')
+        if '.' in alleles:
+            return np.nan
+        elif alleles == '00':
+            return 0.0
+        elif alleles == '01' or alleles == '10':
+            return 1.0
+        elif alleles == '11':
+            return 2.0
+        else:
+            return np.nan
+
+    @staticmethod
+    def _flag_is_phased(flag):
+        if flag & 0b000001 == 1:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def parse_header(header_string):
+        '''
+        Parses a header line into a list of tuples: (tag, key, val)
+        Example
+        -------
+        '##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+        gets parsed into:
+            [('FORMAT', 'ID', 'GQ'),
+             ('FORMAT', 'Number', '1'),
+             ('FORMAT', 'Type', 'Integer'),
+             ('FORMAT', 'Description', '"Genotype Quality"')]
+
+        '''
+        header_string = header_string.strip('#')
+        tag = None
+        keys = []
+        vals = []
+        current = ''
+        in_str = False
+        str_delim = None
+        for i,char in enumerate(header_string):
+            if in_str:
+                if (char == '"' or char == '"') and header_string[i-1] != '\\': 
+                    current += char
+                    vals.append(current)
+                    current = ''
+                    in_str = False
+                else:
+                    current += char
+            elif char == '"' or char == "'":
+                current += char
+                in_str = True
+            elif char == '=':
+                if tag == None:
+                    tag = current
+                else:
+                    if len(keys) == len(vals):
+                        keys.append(current)
+                    else:
+                        raise ValueError('fuck')
+                current = ''
+            elif char == ',' or char == '>' or char == '<':
+                if in_str:
+                    current += char
+                elif len(vals) == len(keys)-1:
+                    vals.append(current)
+                    current = ''
+            else:
+                current += char 
+        # Clean up the loose ends
+        if current != '':
+            vals.append(current)
+        if len(vals) == 1:
+            keys.append(None)
+        return [ (tag,x,y) for x,y in zip(keys,vals)]
 
     def _initialize_tables(self):
         cur = self._db.cursor()
@@ -220,88 +406,23 @@ class VarDab(Freezable):
             FILEID INT,
             VARIANTID INT,
             SAMPLEID INT,
-            FMTID INT,
-            value TEXT,
+            flag INT,      -- bit flag containing information for the variant 
+            dosage FLOAT,
+            PRIMARY KEY(FILEID,VARIANTID,SAMPLEID),
             FOREIGN KEY(FILEID) REFERENCES files(FILEID),
             FOREIGN KEY(VARIANTID) REFERENCES variants(VARIANTID),
-            FOREIGN KEY(SAMPLEID) REFERENCES samples(SAMPLEID),
-            FOREIGN KEY(FMTID) REFERENCES format(FMTID)
+            FOREIGN KEY(SAMPLEID) REFERENCES samples(SAMPLEID)
         ); 
-        ''')
-        cur.execute('''
-            CREATE INDEX IF NOT EXISTS genotypes_format ON genotypes(FMTID)
         ''')
 
         # Create some views
         cur.execute(''' 
             CREATE VIEW IF NOT EXISTS sample_genotypes AS
             SELECT 
-             FILEID, chrom,pos,alt,ref,
-             sample, format, value 
+             chrom,pos,ref,alt,
+             sample, dosage, flag
             FROM genotypes 
-            INNER JOIN variants on genotypes.VARIANTID = variants.VARIANTID
-            INNER JOIN samples on genotypes.SAMPLEID = samples.SAMPLEID
-            INNER JOIN format on genotypes.FMTID = format.FMTID
+            CROSS JOIN variants on genotypes.VARIANTID = variants.VARIANTID
+            CROSS JOIN samples on genotypes.SAMPLEID = samples.SAMPLEID
+            ORDER BY chrom, pos, sample
         ''')
-
-
-    # -----------------------------------------------------------------
-    # Static Methods
-    # -----------------------------------------------------------------
-
-    def parse_header(self,header_string):
-        '''
-        Parses a header line into a list of tuples: (tag, key, val)
-        Example
-        -------
-        '##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
-        gets parsed into:
-            [('FORMAT', 'ID', 'GQ'),
-             ('FORMAT', 'Number', '1'),
-             ('FORMAT', 'Type', 'Integer'),
-             ('FORMAT', 'Description', '"Genotype Quality"')]
-
-        '''
-        header_string = header_string.strip('#')
-        tag = None
-        keys = []
-        vals = []
-        current = ''
-        in_str = False
-        str_delim = None
-        for i,char in enumerate(header_string):
-            if in_str:
-                if (char == '"' or char == '"') and header_string[i-1] != '\\': 
-                    current += char
-                    vals.append(current)
-                    current = ''
-                    in_str = False
-                else:
-                    current += char
-            elif char == '"' or char == "'":
-                current += char
-                in_str = True
-            elif char == '=':
-                if tag == None:
-                    tag = current
-                else:
-                    if len(keys) == len(vals):
-                        keys.append(current)
-                    else:
-                        raise ValueError('fuck')
-                current = ''
-            elif char == ',' or char == '>' or char == '<':
-                if in_str:
-                    current += char
-                elif len(vals) == len(keys)-1:
-                    vals.append(current)
-                    current = ''
-            else:
-                current += char 
-        # Clean up the loose ends
-        if current != '':
-            vals.append(current)
-        if len(vals) == 1:
-            keys.append(None)
-        return [ (tag,x,y) for x,y in zip(keys,vals)]
-
