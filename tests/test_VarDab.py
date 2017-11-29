e88bf582b6724e6e770ec3dcbd1b9298c6bb2d7b
Rob <schae234@umn.edu>
2017-11-29 16:52:24 -0600

    added pytests

---
+++ tests/test_VarDab.py
@@ -0,0 +1,8 @@
+
+def test_VarDab_shape(IncreasingAF,IncreasingAFSwapRef):
+    assert IncreasingAF.shape == (11,5)
+    assert IncreasingAFSwapRef.shape == (11,5)
+
+
+def test_IncreasingAF_Conforms(IncreasingAF,IncreasingAFSwapRef):
+    assert all(IncreasingAF.genotypes(as_dataframe=True) == IncreasingAFSwapRef.genotypes(as_dataframe=True))
