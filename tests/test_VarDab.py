81b6018ce76e22a96ab237b0ba393753b35e3809
Rob <schae234@umn.edu>
2017-12-12 17:28:06 -0600

    add a test to make sure cross join for genotypes is working

--- tests/test_VarDab.py
+++ tests/test_VarDab.py
@@ -8,4 +8,10 @@ def test_IncreasingAF_Conforms(IncreasingAF,IncreasingAFSwapRef):
     assert all(IncreasingAF.genotypes(as_dataframe=True) == IncreasingAFSwapRef.genotypes(as_dataframe=True))
 
 def test_MissingGenotypes(MissingGenotypes):
-    assert True
+    x = MissingGenotypes
+    assert len(x.genotypes()) == len(x.cohort) * len(x.loci)
+
+def test_MissingGenotypesCountNotCrossJoin(MissingGenotypes):
+    x = MissingGenotypes
+    (real,) = x._db.cursor().execute('SELECT COUNT(*) from genotypes').fetchone()
+    assert real == 15
