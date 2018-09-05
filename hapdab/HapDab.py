caa8dc2047a113c02e9bacef5a29946ec6ee94d3
rob <rob@linkage.io>
2018-09-05 14:37:17 -0600

    add stub to conform VCF

--- hapdab/HapDab.py
+++ hapdab/HapDab.py
@@ -42,6 +42,49 @@ class HapDab(Freezable):
         # java -jar ../../hapdab/include/beagle/beagle.08Jun17.d8b.jar gt=2M_CHIP_1000.vcf.gz out=test.phased nthreads=30
         pass
 
+    def _conform_vcf(self,vcf_file):
+        vcf = vcf_file
+        with open(args.out,'w') as OUT:
+            #Clear the header, add the actual chromosomes
+            vcf.header['contig'] = []
+            for chrom in annot._annot.chrom.unique():
+                vcf.header['contig'].append('<ID={}>'.format(chrom))
+            print(vcf.header,file=OUT)
+            for i,variant in enumerate(vcf):
+                # Update the chrom and position to be reflect the MNEc naming format 
+                if variant.id in id_map:
+                    chrom,pos,mnecid = id_map[variant.id]
+                    variant.chrom = chrom
+                    variant.pos = pos
+                    variant.id = mnecid 
+                elif ('{}'.format(variant.chrom),variant.pos) in id_map:
+                    chrom = variant.chrom
+                    pos = variant.pos
+                    if not chrom.startswith('chr'):
+                        chrom = 'chr{}'.format(chrom)
+                    chrom,pos,mnecid = id_map[('{}'.format(chrom),variant.pos)]
+                    variant.chrom = chrom
+                    variant.pos = pos
+                    variant.id = mnecid 
+                else:
+                    continue
+                # Make sure the genotypes are conformed
+                if variant.alt == '.':
+                    variant.alt = REF_map[variant.id][1]
+                try:
+                    variant.conform(REF_map[variant.id][0])
+                except TriAllelicError as e:
+                    continue
+                try:
+                    assert variant.alt == REF_map[variant.id][1]
+                except AssertionError as e:
+                    continue
+                # Fix the borked dot genotpyes
+                for i,x in enumerate(variant.genos):
+                    if x == '.':
+                        variant.genos[i] = './.'
+                print(variant,file=OUT)
+
     def _sort_vcf(self,vcf_file): 
         '''
             Sorts an input vcf based on the fasta
