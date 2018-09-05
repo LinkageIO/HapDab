b9ed72c2e0370f24ac95e3697e8b84d87c96a6a8
rob <rob@linkage.io>
2018-09-05 14:32:41 -0600

    freeze locuspocus req

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     ],
     install_requires = [		
         'minus80>=0.2.0',
-        'locuspocus>=0.1.1',
+        'locuspocus==0.1.1',
         'scipy>=1.1.0',
         'pandas>=0.23.4',
         'numpy>=1.15.0',
