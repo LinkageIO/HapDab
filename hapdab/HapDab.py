4157127c05f644fafecf5383b1bf8a98c9906962
rob <rob@linkage.io>
2018-09-05 22:53:44 -0600

    add docstrings and impute stub

--- hapdab/HapDab.py
+++ hapdab/HapDab.py
@@ -29,25 +29,58 @@ class HapDab(Freezable):
 
     def __init__(self,name,parent=None):
         super().__init__(name)
-        self._initialize_tables()
         if 'Fasta' in self._dict:
             self._fasta = Fasta(self._dict['Fasta'],parent=self)
 
+    # --------------------------------------------
     # Class Methods
+    # --------------------------------------------
 
     @classmethod
-    def from_files(cls,name,vcffile,fastafile):
+    def from_files(cls,name,vcffile,fastafile,skip_phase=False):
         self = cls(name)
         self._add_fasta(fastafile)
-        self._add_ref_vcf(vcffile)
+        self._add_ref_vcf(vcffile,skip_phase=skip_phase)
         return self
 
-    # Private Methods
-
-    def _initialize_tables(self):
+    # --------------------------------------------
+    # Public Methods
+    # --------------------------------------------
+    
+    def impute(self,vcf_file):
+        '''
+            Impute a VCF file to the density of the 
+            Reference VCF
+        '''
         pass
 
+    # --------------------------------------------
+    # Private Methods
+    # --------------------------------------------
+
     def _add_fasta(self,fastafile):
+        '''
+            Add a reference geneome sequence to the database.
+            This reference sequence will be used to sort and 
+            conform the genotype (VCF) files used for imputation.
+            A fasta can only be attached once to a hapdab.
+
+            Parameters
+            ----------
+            fastafile : str, path-like
+                Path to the fasta file
+
+            Returns
+            -------
+            None if successful. See the API for accessing the
+            fasta object.
+
+            Raises
+            ------
+            ValueError if a fasta has already been assigned to the
+            database.
+
+        '''
         if 'Fasta' in self._dict:
             raise ValueError('A Fasta has already been assigned to this database!')
         f = Fasta.from_file(self._m80_name,fastafile,parent=self)
@@ -56,24 +89,36 @@ class HapDab(Freezable):
         self._dict['Fasta'] = self._m80_name
 
     def _add_ref_vcf(self, vcf_file, skip_conform=False, skip_phase=False):
+        '''
+            Add a reference VCF to the database.
+        '''
         original_vcf = vcf_file
-        tmp_files = set()
         if not skip_conform:
             conformed = self._conform_vcf(vcf_file)
-            tmp_files.add(conformed)
             vcf_file = conformed.name
         if not skip_phase:
             phased = self._phase_vcf(vcf_file)
-            tmp_files.add(phased)
             vcf_file = phased.name+'.vcf.gz'
         # Copy the file 
         dest_path = os.path.join(self._basedir,'reference.vcf.gz')
         shutil.copy(vcf_file,dest_path)
-        
+       
 
     def _phase_vcf(self,vcf_file):
+        '''
+            Phase a VCF file using beagle
+
+            Parameters
+            ----------
+            vcf_file : str, path-like
+                A path to a VCF file
+
+            Returns
+            -------
+            A handle to a temporary file containing phased data
+        '''
         # Get a temp file for the output
-        imputed_vcf = self._tmpfile(delete=False)
+        imputed_vcf = self._tmpfile()
         # Get the path of BEAGLE
         beagle_path = pkg_resources.resource_filename(
             'hapdab',
