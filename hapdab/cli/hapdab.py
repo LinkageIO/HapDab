1de07b0f8ababe2bdd95a6accb3d22e0c7a8e300
Rob Schaefer <rob@linkage.io>
2018-09-12 09:50:55 +0200

    explicitly add hapdab cli

--- hapdab/cli/hapdab.py
+++ hapdab/cli/hapdab.py
@@ -4,7 +4,6 @@ import click
 import shutil
 import hapdab as hap
 import minus80 as m80
-from minus80.cli.minus80 import cloud
 
 
 @click.group(epilog=f'Version {hap.__version__}\n{hap.__file__}')
@@ -40,17 +39,20 @@ def create(name,vcf,fasta,skip_phase):
     <vcf>   - The input VCF file
     <fasta> - An input Fasta file
     '''
-    if name is None or vcf is None or fasta is None:
-        click.echo('Invalid Syntax: use --help for more.')
-    else:
-        h = hap.HapDab.from_files(name,vcf,fasta,skip_phase=skip_phase)
+    try:
+        if name is None or vcf is None or fasta is None:
+            click.echo('Invalid Syntax: use --help for more.')
+        else:
+            h = hap.HapDab.from_files(name,vcf,fasta,skip_phase=skip_phase)
+    except Exception as e:
+        import ipdb; ipdb.set_trace()
 cli.add_command(create)
 
 #----------------------------
 #    List Commands
 #----------------------------
 @click.command(short_help='List the available hapdab datasets',
-    help='Reports the available datasets **Frozen** in the minus80 database.'
+    help='Reports the available **Frozen** datasets.'
 )
 @click.option('--name',  default=None,
     help="The name of the dataset you want to check is available. The default value is the wildcard '*' which will return all available datasets with the specified dtype."
@@ -101,6 +103,80 @@ def remap(name, dtype):
 cli.add_command(remap)
 
 
+#----------------------------
+#    Cloud Commands
+#----------------------------
+@click.group()
+def cloud():
+    '''
+    Manage your HapDab datasets in the cloud.
+    '''
+
+cli.add_command(cloud)
+
+@click.command()
+@click.option('--name', metavar='<name>',default=None)
+def list(name):
+    '''List available datasets'''
+    cloud = m80.CloudData()
+    cloud.list(
+        dtype='HapDab',
+        name=name,
+        raw=False
+    )
+
+
+@click.command()
+@click.argument('name', metavar='<name>')
+def push(name):
+    '''
+    \b
+    Push a HapDab dataset to the cloud.
+
+    \b
+    Positional Arguments:
+    <name> - the name of the m80 dataset or raw filename if --raw is set.
+    '''
+    cloud = m80.CloudData()
+    cloud.push(
+        'HapDab',
+        name,
+        raw=False
+    )
+
+@click.command()
+@click.argument('name', metavar='<name>')
+@click.option('--output',default=None,help="Output filename, defaults to <name>. Only valid with --raw")
+def pull(name,output):
+    '''
+    Pull a HapDab dataset from the cloud.
+    '''
+    cloud = m80.CloudData()
+    cloud.pull(
+        'HapDab',
+        name,
+        raw=False,
+        output=output
+    )
+
+@click.command()
+@click.argument('name', metavar='<name>')
+def remove(name):
+    '''
+    Delete a HapDab dataset from the cloud.
+    '''
+    cloud = m80.CloudData()
+    cloud.remove(
+        'HapDab',
+        name,
+        raw=False
+    )
+
+
+cloud.add_command(list)
+cloud.add_command(push)
+cloud.add_command(pull)
+cloud.add_command(remove)
 
 cli.add_command(cloud)
 
