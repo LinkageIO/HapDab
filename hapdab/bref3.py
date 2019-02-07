fb6e1efb2a07eb632883982a7e592c6ddc4c1c48
rob <rob@linkage.io>
2019-02-07 16:15:00 -0700

    implemented part of bref3

--- hapdab/bref3.py
+++ hapdab/bref3.py
@@ -16,13 +16,27 @@ class bref3:
         recList = []
         nRecs = self.read_int()
         while(nRecs != 0):
-            self.readDataBlock(s
+            self.readDataBlock(samples, recList, nRecs)
+            nRecs = self.read_int()
 
+        return recList
+
+    def readDataBlock(self,samples, recList, nRecs):
+        chrom = self.read_utf()
+        nSeqs = self.read_unsigned_short()
+
+        hap2Seq = []
+        for j in range(0,2*len(samples)):
+            hap2Seq.append(self.read_unsigned_short()) 
+        
+        for j in range(0,nRecs):
+            rec = self.readRecord(chrom,samples,nSeqs,hap2Seq)
+        breakpoint()
 
 
     def read_string_array(self):
         length = self.read_int()
-        entries = [self.read_utf().decode('utf-8') for _ in range(length)]
+        entries = [self.read_utf() for _ in range(length)]
         return entries
 
     def read_boolean(self):
@@ -54,7 +68,7 @@ class bref3:
 
     def read_utf(self):
         utf_length = struct.unpack('>H', self.stream.read(2))[0]
-        return self.stream.read(utf_length)
+        return self.stream.read(utf_length).decode('utf-8')
 
     def read_int(self):
         return struct.unpack('>i', self.stream.read(4))[0]
