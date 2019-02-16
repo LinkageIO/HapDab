d3aad07160e97c4b4a4ebc051aa7b6b0bdf12932
rob <rob@linkage.io>
2019-02-15 18:19:33 -0700

    initial implementation of bref3, still needs debugging

--- hapdab/bref3.py
+++ hapdab/bref3.py
@@ -1,9 +1,10 @@
 import struct
+from itertools import permutations
 
 class bref3:
     def __init__(self, filename):
         self.stream = open(filename, 'rb')
-        self.snvPerms = ['A','C','G','T']
+        self.snvPerms = list(permutations(['A','C','G','T']))
 
     def readRecords(self):
         # read the magin number
@@ -21,6 +22,18 @@ class bref3:
 
         return recList
 
+    def read_string_array(self):
+        length = self.read_int()
+        entries = [self.read_utf() for _ in range(length)]
+        return entries
+
+    def readByteLengthStringArray(self):
+        length = self.read_unsigned_byte()
+        array = []
+        for j in range(0,length):
+            array.append(self.read_utf())
+        return array
+
     def readDataBlock(self,samples, recList, nRecs):
         chrom = self.read_utf()
         nSeqs = self.read_unsigned_short()
@@ -31,13 +44,78 @@ class bref3:
         
         for j in range(0,nRecs):
             rec = self.readRecord(chrom,samples,nSeqs,hap2Seq)
-        breakpoint()
+            recList.append(rec)
+            breakpoint()
 
+    def readRecord(self, chrom, samples, nSeqs, hap2Seq):
+        marker = self.readMarker(chrom)
+        coding = self.read_byte()
+        if coding == 0:
+            return self.readSeqCodedRecord(samples,marker,nSeqs,hap2Seq)
+        elif coding == 1:
+            return self.readAlleleCodedRecord(samples, marker)
 
-    def read_string_array(self):
+    def readSeqCodedRecord(self,samples,marker,nSeqs,hap2Seq):
+        seq2Allele = []
+        for j in range(0,nSeqs):
+            seq2Allele.append(self.read_insigned_byte())
+        hap2Allele = []
+        for j in range(0,len(hap2Seq)):
+            hap2Allele.append(seq2Allele[hap2Seq[j]])
+        record = dict()
+        record['marker'] = marker
+        record['samples'] = samples
+        record['hap2Allele'] = hap2Allele
+        return record
+
+    def readAlleleCodedRecord(self,samples,marker):
+        nHaps = 2*len(samples)
+        nAlleles = len(marker['alleles'])
+        hapIndices = []
+        majorAllele = -1
+        for j in range(0,nAlleles):
+            hapIndices.append(self.readIntArray())
+            if hapIndices[j] is None:
+                majorAllele = j
+
+        hap2Allele = []
+        for j in range(0,nHaps):
+            hap2Allele.append(majorAllele)
+        for j in range(0,len(hapIndices)):
+            if hapIndices[j] != None:
+                for hap in hapIndices[j]:
+                    hap2Allele[hap] = j
+        record = dict()
+        record['marker'] = marker
+        record['samples'] = samples
+        record['hapToAllele'] = hap2Allele
+        return record
+
+    def readIntArray(self):
         length = self.read_int()
-        entries = [self.read_utf() for _ in range(length)]
-        return entries
+        if length == -1:
+            return None
+        else:
+            array = []
+            for j in range(0,length):
+                array.append(self.read_int())
+            return array
+
+    def readMarker(self, chrom):
+        marker = dict()
+        marker['pos'] = self.read_int()
+        marker['id'] = self.readByteLengthStringArray()
+
+        alleleCode = self.read_byte()
+        if alleleCode == -1:
+            marker['alleles'] = self.readStringArray()
+            marker['end'] = self.read_int()
+        else:
+            marker['nAlleles'] = 1 + (alleleCode & 0b11)
+            permIndex = (alleleCode >> 2)
+            marker['alleles'] = self.snvPerms[permIndex][0:marker['nAlleles']]
+            marker['end'] = -1
+        return marker
 
     def read_boolean(self):
         return struct.unpack('?', self.stream.read(1))[0]
